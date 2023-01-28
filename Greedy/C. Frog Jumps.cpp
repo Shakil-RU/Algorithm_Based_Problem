1094611d09730646e5003ad6a965304823dacdcd
Shakil hossan <shakilnirob111@gmail.com>
2023-01-28 23:29:33 +0600

    Update C. Frog Jumps.cpp

--- Greedy/C. Frog Jumps.cpp
+++ Greedy/C. Frog Jumps.cpp
@@ -1,43 +1,24 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define endl "\n";
+#define ll long long
 using namespace std;
-
-
-void solve() {
- 
-   string s; cin>>s;
- 
-   
-   vector<int>v;
-   v.push_back(0);
-   for(int i=0;i<s.size();i++){
-      if(s[i]=='R'){
-         v.push_back(i+1);
+int main() {
+    int t;
+  cin>>t;
+  while(t--) {
+    string s;
+    cin>>s;
+    int index = -1;
+    int jump = 0;
+    int cell = s.length();
+    for(int i = 0; i < cell; i++) {
+      if(s[i] == 'R') {
+        jump = max(i - index, jump);
+        index = i;
       }
-   }
- 
-   v.push_back(s.size()+1);
- 
-   int ans(0);
- 
-   for(int i=0;i<v.size()-1;i++){
-      ans=max(ans,v[i+1]-v[i]);
-   }
- 
-   cout << ans << endl;
- 
- 
-}
- 
-int main() 
-{
-   
-   long long t; cin>>t;
-   while(t--) {
- 
-      solve();
- 
-   }
- 
- 
-	return 0;
+    }
+    jump = max(cell-index, jump);
+    cout<<jump<<endl
+  }
+  return 0;
 }
