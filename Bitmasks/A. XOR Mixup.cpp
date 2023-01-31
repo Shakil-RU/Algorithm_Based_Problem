f6372c53234c42732d990ca9d08620c36e5c8526
Shakil hossan <shakilnirob111@gmail.com>
2023-01-31 09:09:58 +0600

    Create A. XOR Mixup.cpp

---
+++ Bitmasks/A. XOR Mixup.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int n; cin>>n;
+        int arr[n+5];
+        for(int i = 0; i<n; i++)
+        {
+            cin>>arr[i];
+        }
+        cout<<arr[0]<<endl;
+    }
+    return 0;
+}
